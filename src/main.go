9317d256840daf1b1f85d7a3085617888aad24ec
Carlos Vasquez <cvasquezr@outlook.com>
2021-07-05 20:58:58 -0500

    Variables en go

--- src/main.go
+++ src/main.go
@@ -3,5 +3,30 @@ package main
 import "fmt"
 
 func main() {
+	// Declaración de constantes
+	const pi float64 = 3.14
+	// Declaración de constantes otra manera
+	const pi2 = 3.141516
+	fmt.Println(pi, pi2)
+	fmt.Println("pi:", pi)
+	fmt.Println("pi2:", pi2)
 	fmt.Println("Hola mundo")
+
+	//Declaración e vairables enteras
+	//Incializa sin declarar variable
+	base := 12
+	//Declara e inicializa
+	var altura int = 14
+	//Declara, pero no se inicializa
+	var area int
+	area = base * altura
+	fmt.Println(area)
+
+	//Zero value, valor por defecto
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
 }
